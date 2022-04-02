d5f290626393951252f04a4de59b5a361de569a8
usvasiliy <drogovozov96@gmail.com>
2022-04-02 21:40:22 +0300

    learn crud with PostModel

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Post;
+
+class PostController extends Controller
+{
+    public function index() {
+      $posts = Post::all();
+
+      dump($posts);
+      dd('end');
+    }
+
+    public function create() {
+      Post::create([
+        'title' => 'title of post from crud',
+        'content' => 'content of post from crud',
+        'image' => 'ercrud.jpg',
+      ]);
+      dd('created');
+    }
+
+
+    public function update() {
+      $post = Post::find(3);
+      $post->update([
+        'title' => 'update',
+        'content' => 'update123',
+        'image' => 'update123',
+        'likes' => 2250,
+      ]);
+      dd('updated');
+    }
+
+    public function delete() {
+      // $post = Post::find(1);  <----- удаление спомощью софтделита
+      // $post->delete();
+
+      // $post = Post::withTrashed()->find(1); <--- восстановление
+      // $post->restore();
+      dd('delete');
+    }
+
+    public function firstOrCreate() {
+
+      $post = Post::firstOrCreate([
+        'title' => '111title of post from crud'
+      ],[
+        'title' => '111title of post from crud',
+        'content' => 'content1111',
+        'image' => 'image1111.jpg'
+      ]);
+      dd($post->title);
+    }
+
+
+
+    public function updateOrCreate() {
+      $post = Post::updateOrCreate([
+        'title' => '111title of post from crud'
+      ],[
+        'title' => 'createtetitle of post from crud',
+        'content' => 'createcontent1111',
+        'image' => 'createimage1111.jpg'
+      ]);
+      dd('updateOrCreate');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 use App\Models\Page;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\MyPlaceController;
+use App\Http\Controllers\PostController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,4 +15,14 @@ use App\Http\Controllers\MyPlaceController;
 
 
 
-Route::get('/', [MyPlaceController::class, 'index']);
+Route::get('/posts', [PostController::class, 'index']);
+
+Route::get('/posts/create', [PostController::class, 'create']);
+
+Route::get('/posts/update', [PostController::class, 'update']);
+
+Route::get('/posts/delete', [PostController::class, 'delete']);
+
+Route::get('/posts/first_or_create', [PostController::class, 'firstOrCreate']);
+
+Route::get('/posts/update_or_create', [PostController::class, 'updateOrCreate']);
